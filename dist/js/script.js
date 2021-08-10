3b60571ab17b066e9ef4ddf0c282d39a0baefef2
Pavel Rybakin <pvrybakin@gmail.com>
2021-08-10 21:41:40 +0300

    js mod

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,36 @@
 window.addEventListener('DOMContentLoaded', () =>{
     const hamburger = document.querySelector('.header__hamburger'),
-          menu = document.querySelector('.header__list');
+          menu = document.querySelector('.header__list'),
+          span = document.querySelectorAll('.header__hamburger span');
     
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('header__hamburger_active');
-        menu.classList.toggle('header__list_active');
+    function menuShowAndHide(i) {
+        i.addEventListener('click', () => {
+            if(!i.classList.contains('header__hamburger_active') && !menu.classList.add('header__list_active')) {
+                i.classList.add('header__hamburger_active');
+                menu.classList.add('header__list_active');
+            } else {
+                i.classList.remove('header__hamburger_active');
+                menu.classList.remove('header__list_active');
+            }
+            
+        });
+    }
+    menuShowAndHide(hamburger);
+    menuShowAndHide(span);
+
+    
+    document.addEventListener('click', (event) =>{
+        let target = event.target,
+            itsMenu = target == menu || menu.contains(target),
+            itsHamburger = target == hamburger,
+            menuIsActive = menu.classList.contains('header__list_active');
+
+            if (!itsMenu && !itsHamburger && menuIsActive){
+                hamburger.classList.remove('header__hamburger_active');
+                menu.classList.remove('header__list_active');
+            } 
     });
+   
     console.log('dfslem');
 
 });
